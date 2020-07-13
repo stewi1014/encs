ea91d625843c0ce1646b42ed1f436d924668b0a3
Stewart Borle <stewi1014@gmail.com>
2020-07-13 17:49:22 +0200

    Almost there

--- encodable/encodable_test.go
+++ encodable/encodable_test.go
@@ -1,102 +1,39 @@
 package encodable_test
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
-	"unsafe"
-
-	"github.com/stewi1014/encs/encio"
 )
-
-const minSingleInt = int8(-1<<7 + 9)
-
-// Decode implements Encodable.
-func DecodeInt(ptr unsafe.Pointer) error {
-
-	*(*int)(ptr) = int(1)
-
-	return nil
-}
 
 const nilPointer = -1
 
-// Decode implements Encodable.
-func DecodeMap(ptr unsafe.Pointer) error {
-	l := 1
-	m := reflect.NewAt(rMapType, ptr).Elem()
-
-	if l == nilPointer {
-		m.Set(reflect.New(rMapType).Elem())
-		return nil
-	}
-
-	if uintptr(l)*(rMapType.Key().Size()+rMapType.Elem().Size()) > encio.TooBig {
-		return encio.NewIOError(encio.ErrMalformed, nil, fmt.Sprintf("map size of %v is too big", l), 0)
-	}
-
-	v := reflect.MakeMapWithSize(rMapType, int(l))
+func DecodeMap(m reflect.Value) error {
+	v := reflect.MakeMapWithSize(rMapType, int(1))
 	m.Set(v)
 
 	nKey := reflect.New(rMapType.Key()).Elem()
-	err := DecodeInt(unsafe.Pointer(nKey.UnsafeAddr()))
-	if err != nil {
-		return err
-	}
+	nKey.Set(reflect.ValueOf(int(1)))
 
 	nVal := reflect.New(rMapType.Elem()).Elem()
-	err = DecodeSecond(unsafe.Pointer(nVal.UnsafeAddr()))
-	if err != nil {
-		return err
-	}
+	nVal.Field(0).Set(m)
+	nVal.Field(1).Set(reflect.ValueOf(int(1)))
 
 	v.SetMapIndex(nKey, nVal)
 
 	return nil
 }
 
-// Decode implements Encodable.
-func Decode(ptr unsafe.Pointer) error {
-	// I really don't like doubling up on the Struct encodable, but wow,
-	// what a difference it makes.
+func Decode(v reflect.Value) error {
 
-	f0Offset := rStructType.Field(0).Offset
-	f1Offset := rStructType.Field(1).Offset
+	v.Field(1).Set(reflect.ValueOf(int(1)))
 
-	mptr := unsafe.Pointer(uintptr(ptr) + f0Offset)
-	seenMap = &mptr
-	fmt.Println(mptr)
-	if err := DecodeMap(mptr); err != nil {
-		return err
-	}
-	if err := DecodeInt(unsafe.Pointer(uintptr(ptr) + f1Offset)); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Decode implements Encodable.
-func DecodeSecond(ptr unsafe.Pointer) error {
-	// I really don't like doubling up on the Struct encodable, but wow,
-	// what a difference it makes.
-
-	f0Offset := rStructType.Field(0).Offset
-	f1Offset := rStructType.Field(1).Offset
-
-	mptr := unsafe.Pointer(uintptr(ptr) + f0Offset)
-	reflect.NewAt(rMapType, mptr).Elem().Set(reflect.NewAt(rMapType, *seenMap).Elem())
-
-	if err := DecodeInt(unsafe.Pointer(uintptr(ptr) + f1Offset)); err != nil {
-		return err
-	}
+	DecodeMap(v.Field(0))
 	return nil
 }
 
 var (
 	rStructType = reflect.TypeOf(RecursiveTest3{})
 	rMapType    = reflect.TypeOf(RecursiveTest3{}.A)
-
-	seenMap *unsafe.Pointer
 )
 
 type RecursiveTest3 struct {
@@ -119,7 +56,7 @@ func TestRecursiveTypes(t *testing.T) {
 	val := reflect.ValueOf(encode).Elem()
 
 	decodedValue := reflect.New(val.Type()).Elem()
-	err := Decode(unsafe.Pointer(decodedValue.UnsafeAddr()))
+	err := Decode(decodedValue)
 	if err != nil {
 		t.Error(err)
 	}

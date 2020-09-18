1339e258aae575026f2618c7e08ed609f0a098e7
Stewart Borle <stewi1014@gmail.com>
2020-09-18 19:02:44 +0200

    Split Pipe into Reader and Writer

--- encio/pipe_test.go
+++ encio/pipe_test.go
@@ -6,22 +6,23 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stewi1014/encs/encio"
 )
 
 func TestPipe(t *testing.T) {
 	rng := rand.New(rand.NewSource(0))
-	count := 20
+	count := 100
 	maxBuffer := 500
 
-	p := encio.Pipe()
+	pr, pw := encio.Pipe()
 
 	read := make(chan int64)
 	var written int64
 
-	r := encio.NewChecksumReader(p, nil)
-	w := encio.NewChecksumWriter(p, nil)
+	r := encio.NewChecksumReader(pr, nil)
+	w := encio.NewChecksumWriter(pw, nil)
 
 	go t.Run("Pipe Read", func(t *testing.T) {
 		n, err := io.Copy(ioutil.Discard, r)
@@ -32,6 +33,9 @@ func TestPipe(t *testing.T) {
 	})
 
 	for i := 0; i < count; i++ {
+		// Add a small random delay so the synchronisation logic is tested better.
+		time.Sleep(time.Duration(rng.Intn(10000)) * time.Microsecond)
+
 		buff := make([]byte, rng.Intn(maxBuffer-1)+1)
 		_, err := rng.Read(buff)
 		if err != nil {
@@ -46,7 +50,8 @@ func TestPipe(t *testing.T) {
 		written += int64(n)
 	}
 
-	p.Close()
+	pw.Close()
+	pr.Close()
 
 	totalRead := <-read
 
